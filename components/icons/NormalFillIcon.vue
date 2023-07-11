14c554033f15b0135a35d0f1b9a5fa2d466fe3cd
shunsuke <shun.8000.sf@gmail.com>
2023-07-11 20:46:47 +0900

    change icon

--- components/icons/NormalFillIcon.vue
+++ components/icons/NormalFillIcon.vue
@@ -1,8 +1,14 @@
 <template>
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect x="9.33334" y="4" width="5.33333" height="5.33333" class="btn-icon-fill" />
-    <rect x="4" y="9.33334" width="5.33333" height="5.33333" class="btn-icon-fill" />
-    <rect x="9.33334" y="14.6667" width="5.33333" height="5.33333" class="btn-icon-fill" />
-    <rect x="14.6667" y="9.33334" width="5.33333" height="5.33333" class="btn-icon-fill" />
+    <rect x="9.66669" y="5" width="4.66667" height="4.66667" class="btn-icon-fill" />
+    <rect x="5" y="9.66669" width="4.66667" height="4.66667" class="btn-icon-fill" />
+    <rect x="9.66669" y="14.3333" width="4.66667" height="4.66667" class="btn-icon-fill" />
+    <rect x="14.3333" y="9.66669" width="4.66667" height="4.66667" class="btn-icon-fill" />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M20 3H4C3.44772 3 3 3.44772 3 4V20C3 20.5523 3.44772 21 4 21H20C20.5523 21 21 20.5523 21 20V4C21 3.44772 20.5523 3 20 3ZM4 2C2.89543 2 2 2.89543 2 4V20C2 21.1046 2.89543 22 4 22H20C21.1046 22 22 21.1046 22 20V4C22 2.89543 21.1046 2 20 2H4Z"
+      class="btn-icon-fill"
+    />
   </svg>
 </template>
